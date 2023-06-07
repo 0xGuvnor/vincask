f60372399537755b0df2bab7318734e4ec0ef30c
0xpaprhnds <yokeyeong@me.com>
2023-06-07 11:33:10 +0200

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,12 +6,16 @@ import { useMediaQuery } from "react-responsive";
 
 const Navbar = () => {
   const isMobileOrTablet = useMediaQuery({ maxWidth: 768 });
-  const isScrolled = isMobileOrTablet ? useScroll(10) : useScroll(150);
+  const isMobileOrTabletScrolled = useScroll(10);
+  const isDesktopScrolled = useScroll(200);
 
   return (
     <nav
       className={`${
-        isScrolled && "bg-base-100/90 backdrop-blur-md shadow-xl"
+        isMobileOrTablet
+          ? isMobileOrTabletScrolled &&
+            "bg-base-100/90 backdrop-blur-md shadow-xl"
+          : isDesktopScrolled && "bg-base-100/90 backdrop-blur-md shadow-xl"
       } fixed inset-x-0 top-0 flex items-center justify-between h-12 px-2 md:px-8 transition duration-700 ease-in-out md:h-16 text-neutral-content z-50`}
     >
       <div className="flex justify-start w-1/4">
