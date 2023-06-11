0376c9e9e36319700d509278f436e3b152bb3d20
0xpaprhnds <yokeyeong@me.com>
2023-06-11 20:06:15 +0200

    Update DesktopMenu.tsx

--- src/components/Navbar/DesktopMenu.tsx
+++ src/components/Navbar/DesktopMenu.tsx
@@ -4,7 +4,7 @@ const DesktopMenu = () => {
   return (
     <div className="items-center hidden space-x-6 md:flex">
       {menuItems.map((menuItem) => (
-        <div className="relative cursor-pointer group">
+        <div key={menuItem} className="relative cursor-pointer group">
           <span className="">{menuItem}</span>
           <span className="absolute inset-x-0 bottom-0 -z-10 w-full h-full transition duration-300 ease-in-out origin-bottom scale-y-[0.05] group-hover:scale-y-110 bg-primary"></span>
         </div>
