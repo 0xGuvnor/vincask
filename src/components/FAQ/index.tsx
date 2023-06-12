f39cbefed88c76357f0a1de7780d5e5494359345
0xpaprhnds <yokeyeong@me.com>
2023-06-12 02:50:05 +0200

    Update index.tsx

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Container from "../Container";
 import Heading from "../Heading";
 import Accordian from "./Accordian";
@@ -11,12 +12,12 @@ const FAQ = () => {
           <h3 className="text-sm md:text-base md:max-w-3xl">
             Have a different question and can&apos;t find the answer you&apos;re
             looking for?{" "}
-            <a
+            <Link
               href="/contact"
               className="underline transition-colors duration-200 ease-in-out cursor-pointer decoration-blue-500 underline-offset-[3px] decoration-1 hover:text-blue-500"
             >
               Reach out to our support team
-            </a>{" "}
+            </Link>{" "}
             and we&apos;ll get back to you as soon as we can.
           </h3>
         }
