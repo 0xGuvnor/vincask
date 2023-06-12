28f9ea1e9e8be4cb3e6bd159c74234d9e65f7bb4
0xpaprhnds <yokeyeong@me.com>
2023-06-13 00:43:57 +0200

    Update ContactCard.tsx

--- src/components/ContactCards/ContactCard.tsx
+++ src/components/ContactCards/ContactCard.tsx
@@ -8,7 +8,7 @@ const ContactCard = ({ type, email, phone }: Props) => {
   return (
     <div className="flex flex-col px-4 py-2 space-y-4 border-l-[1px] relative border-gray-400 w-56">
       <h3 className="text-2xl font-black">{type}</h3>
-      <span className="absolute -top-4 -left-[1px] w-[1px] h-12 bg-primary z-50"></span>
+      <span className="absolute -top-4 -left-[1px] w-[1px] h-12 bg-primary z-30"></span>
 
       <div className="space-y-1">
         <a
