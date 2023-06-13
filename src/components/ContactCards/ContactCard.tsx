5d49a2b4e770a3df8cad5c74f1b9fd9d33d51095
0xpaprhnds <yokeyeong@me.com>
2023-06-13 23:49:37 +0200

    Update ContactCard.tsx

--- src/components/ContactCards/ContactCard.tsx
+++ src/components/ContactCards/ContactCard.tsx
@@ -7,10 +7,12 @@ interface Props {
 const ContactCard = ({ type, email, phone }: Props) => {
   return (
     <div className="flex flex-col px-4 py-2 space-y-4 border-l-[1px] relative border-gray-400 w-56">
-      <h3 className="text-2xl font-black">{type}</h3>
+      <h3 className="overflow-x-visible text-lg font-black md:text-2xl">
+        {type}
+      </h3>
       <span className="absolute -top-4 -left-[1px] w-[1px] h-12 bg-primary z-30"></span>
 
-      <div className="space-y-1">
+      <div className="space-y-1 text-sm md:text-base">
         <a
           href={`mailto:${email}`}
           className="text-secondary brightness-150 hover:brightness-100"
