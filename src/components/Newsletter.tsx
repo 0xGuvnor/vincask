a33f24a7f368629a51ad74c6fd153a8219ce2ffe
0xpaprhnds <yokeyeong@me.com>
2023-06-15 00:17:23 +0200

    Update Newsletter.tsx

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -25,44 +25,46 @@ const Newsletter = () => {
   };
 
   return isMounted ? (
-    <Container classNames="md:flex-row justify-between items-center w-full !gap-10">
-      <div className="w-full text-3xl md:text-5xl sm:w-auto">
-        <h3>
-          Want product news and updates? {!isMobile && <br />}Sign up for our
-          newsletter.
-        </h3>
-      </div>
+    <div className="border-y-[0.5px] border-white/30">
+      <Container classNames="md:flex-row justify-between items-center w-full !gap-10">
+        <div className="w-full text-3xl md:text-5xl sm:w-auto">
+          <h3>
+            Want product news and updates? {!isMobile && <br />}Sign up for our
+            newsletter.
+          </h3>
+        </div>
 
-      <div className="relative flex-1 w-full">
-        <form onSubmit={handleSubmit(onSubmit)} className="flex gap-4">
-          <NewsletterInput
-            label="Enter your email address"
-            register={register}
-            errors={errors}
-            type="email"
-            id="email"
-          />
+        <div className="relative flex-1 w-full space-y-2 md:space-y-0">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex gap-4">
+            <NewsletterInput
+              label="Enter your email address"
+              register={register}
+              errors={errors}
+              type="email"
+              id="email"
+            />
 
-          <button
-            type="submit"
-            className="px-4 h-[38px] md:text-base text-sm md:!h-[42px] py-2 normal-case transition-colors duration-300 ease-in-out rounded btn btn-sm bg-primary hover:bg-primary-focus"
-          >
-            Subscribe
-          </button>
-        </form>
+            <button
+              type="submit"
+              className="px-4 h-[38px] md:text-base text-sm md:!h-[42px] py-2 normal-case transition-colors duration-300 ease-in-out rounded btn btn-sm bg-primary hover:bg-primary-focus"
+            >
+              Subscribe
+            </button>
+          </form>
 
-        <p className="absolute left-0 text-xs md:text-sm -bottom-7">
-          We care about your data. Read our{" "}
-          <Link
-            href="/"
-            className="underline transition-colors duration-300 ease-in-out cursor-pointer hover:text-blue-500 decoration-primary underline-offset-[3px]"
-          >
-            privacy policy
-          </Link>
-          .
-        </p>
-      </div>
-    </Container>
+          <p className="left-0 text-xs md:absolute md:text-sm -bottom-7">
+            We care about your data. Read our{" "}
+            <Link
+              href="/"
+              className="underline transition-colors duration-300 ease-in-out cursor-pointer hover:text-blue-500 decoration-primary underline-offset-[3px]"
+            >
+              privacy policy
+            </Link>
+            .
+          </p>
+        </div>
+      </Container>
+    </div>
   ) : null;
 };
 export default Newsletter;
