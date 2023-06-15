0fb8128de5ce4c0f093639a3c02851cbc298e419
0xpaprhnds <yokeyeong@me.com>
2023-06-16 01:21:03 +0200

    Update Overlay.tsx

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,6 +1,6 @@
 const Overlay = () => {
   return (
-    <div className="fixed inset-x-0 top-0 z-40 h-screen bg-white/5 backdrop-blur-sm"></div>
+    <div className="fixed inset-x-0 top-0 z-40 h-screen bg-white/5 backdrop-blur-sm brightness-75"></div>
   );
 };
 export default Overlay;
