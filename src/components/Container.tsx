db36fe2b702de34211379a9682c93ab9845fd3e0
0xpaprhnds <yokeyeong@me.com>
2023-08-10 12:34:35 +0200

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,7 +10,7 @@ const Container = ({ children, id, classNames }: Props) => {
   return (
     <section
       id={id}
-      className={`flex flex-col gap-12 px-4 py-14 md:px-12 md:py-20 md:gap-16 2xl:px-0 max-w-[1500px] mx-auto w-full ${classNames}`}
+      className={`flex flex-col gap-12 px-4 py-16 md:px-12 md:py-24 md:gap-16 2xl:px-0 max-w-[1500px] mx-auto w-full ${classNames}`}
     >
       {children}
     </section>
