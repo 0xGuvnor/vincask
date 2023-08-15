e00e9c6e7b5e29dfefb8bdafc21083e176262df1
0xpaprhnds <yokeyeong@me.com>
2023-08-15 13:04:11 +0200

    Update CtaButtons.tsx

--- src/components/Hero/CtaButtons.tsx
+++ src/components/Hero/CtaButtons.tsx
@@ -1,30 +1,27 @@
-import { Open_Sans } from "next/font/google";
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
-
-// const openSans = Open_Sans({ subsets: ["latin"] });
 
 const CtaButtons = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-2 mt-4 md:gap-12 md:flex-row md:mt-6">
       <button type="button" className="focus:outline-none">
-        <a
-          href="#_"
+        <Link
+          href="/nft"
           className="relative inline-flex items-center justify-center w-32 px-4 py-3 overflow-hidden text-sm font-medium tracking-tighter text-white rounded md:text-base bg-primary md:w-40 group"
         >
           <span className="absolute w-0 h-0 transition-all duration-500 ease-out rounded-full bg-primary-focus group-hover:w-56 group-hover:h-56"></span>
           <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-[#ED9121]"></span>
           <span className="relative text-base font-semibold md:text-xl text-primary-content">
-            Reserve now
+            Mint now
           </span>
-        </a>
+        </Link>
       </button>
 
       <button
         type="button"
         className="relative group hover:text-white focus:outline-none"
       >
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/whisky" className="flex items-center gap-2">
           <span className="text-base md:text-xl">Learn more</span>
           <FaArrowRight className="inset-y-0 h-full transition-all duration-300 ease-in-out md:absolute -right-6 group-hover:-right-7" />
 
