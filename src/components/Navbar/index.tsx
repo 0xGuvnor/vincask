51c38746fdbd8516242809573615a4a16928c524
0xpaprhnds <yokeyeong@me.com>
2023-08-17 11:57:36 +0200

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       className={`${
         isMobileOrTablet
           ? isMobileOrTabletScrolled &&
-            "bg-base-100/75 backdrop-blur-sm shadow-xlx"
+            "bg-base-100/75 backdrop-blur-sm shadow-xl"
           : isDesktopScrolled && "bg-base-100/75 backdrop-blur-sm shadow-xl"
       } fixed inset-x-0 top-0 flex items-center justify-between h-14 px-2 md:px-12 transition duration-300 ease-in-out md:h-20 text-neutral-content z-50`}
     >
