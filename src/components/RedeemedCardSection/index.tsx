25a4c6e4f4a5ddf0a7f189e63e4fe8465dd49228
0xpaprhnds <yokeyeong@me.com>
2023-08-20 23:06:12 +0200

    Update index.tsx

--- src/components/RedeemedCardSection/index.tsx
+++ src/components/RedeemedCardSection/index.tsx
@@ -62,9 +62,9 @@ const RedeemedCardSection = ({ defaultImg }: Props) => {
             nftData={nftData}
           />
         ))} */}
-        {cardArr.map((nftData, id) => (
+        {cardArr.map((x, id) => (
           <RedeemedCard
-            key={nftData.tokenId}
+            key={id}
             defaultImg={defaultImg}
             nftData={nftDataArr ? nftDataArr[id] : { title: "", tokenId: "" }}
           />
