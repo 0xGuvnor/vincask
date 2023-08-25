59a05d78e9ee5085cd51d9fda928d031a518f43c
0xpaprhnds <yokeyeong@me.com>
2023-08-25 04:12:21 +0200

    Update messageToSign.ts

--- src/constants/messageToSign.ts
+++ src/constants/messageToSign.ts
@@ -1,11 +1,9 @@
 export const messageToSign = (
   address: `0x${string}` | undefined,
   datetime: Date
-) => {
-  return `Signing this message allows Vincask to confirm your ownership of wallet address ${address}, and that you agree to the terms & conditions.
+) => `Signing this message allows Vincask to confirm your ownership of wallet address ${address}, and that you agree to the terms & conditions.
 
   Time: ${datetime.toLocaleTimeString()} 
   Date: ${datetime.toLocaleDateString()} 
   
   * This is a gasless transaction *`;
-};
