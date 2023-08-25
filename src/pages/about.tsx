061c12a82f72d5a4dd0e3eb23f916959bee76aee
0xpaprhnds <yokeyeong@me.com>
2023-08-26 01:50:56 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -56,15 +56,19 @@ export default About;
 export const getStaticProps: GetStaticProps = async () => {
   const {
     data: { collageImages },
-  } = await axios.get(`${baseUrl}api/getCollageImages`);
+  } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}api/getCollageImages`
+  );
 
   const {
     data: { timelineItems },
-  } = await axios.get(`${baseUrl}api/getTimelineItems`);
+  } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}api/getTimelineItems`
+  );
 
   const {
     data: { companyInfos },
-  } = await axios.get(`${baseUrl}api/getCompanyInfo`);
+  } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}api/getCompanyInfo`);
 
   return { props: { collageImages, timelineItems, companyInfos } };
 };
