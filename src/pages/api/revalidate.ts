5f7a57910253667eeb833957a48f0b30266c5751
0xpaprhnds <yokeyeong@me.com>
2023-08-26 15:26:47 +0200

    Added ISR for updates to Supabase

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -10,7 +10,11 @@ export default async function handler(
 
   try {
     await res.revalidate("/");
-    // await res.revalidate("/about");
+    await res.revalidate("/about");
+    await res.revalidate("/contact");
+    await res.revalidate("/nft");
+    await res.revalidate("/redeem");
+    await res.revalidate("/whisky");
 
     res.status(200).json({ revalidated: true });
   } catch (error) {
