500f4e0beafcd24ef273f66a6a437ef9dfe312a8
0xpaprhnds <yokeyeong@me.com>
2023-08-30 19:00:40 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -28,7 +28,7 @@ const About = ({
       <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-[#2A3439]/40 from-0% -z-10"></div>
 
       <Container classNames="flex-1 overflow-x-hidden xl:overflow-x-visible relative">
-        <div className="flex flex-col justify-start items-center md:justify-center min-h-[97vh] md:min-h-[75vh] md:flex-row space-y-8 md:space-y-0 md:space-x-12">
+        <div className="flex flex-col justify-start items-center md:justify-center min-h-[110vh] md:min-h-[75vh] md:flex-row space-y-8 md:space-y-0 md:space-x-12">
           <div className="flex flex-col md:min-w-[670px] mt-6 space-y-2 text-center md:max-w-3xl md:space-y-4 md:text-left md:mt-0">
             <h1 className="text-3xl font-black text-white font-header md:text-5xl">
               Our mission is to redefine the world of whisky and art.
