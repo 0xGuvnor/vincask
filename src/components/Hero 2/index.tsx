6844b95e7840afd1e2a57f6173ac6fd605b777d6
0xpaprhnds <yokeyeong@me.com>
2023-09-01 20:40:17 +0200

    Update index.tsx

--- src/components/Hero 2/index.tsx
+++ src/components/Hero 2/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import CtaButtons from "./CtaButtons";
 import useScroll from "@/hooks/useScroll";
 import { AnimatePresence, motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 
 interface Props {
   heroImage: string;
@@ -9,22 +10,23 @@ interface Props {
 
 const Hero2 = ({ heroImage }: Props) => {
   const isScrolled = useScroll(25);
+  const isMobileOrTablet = useMediaQuery({ maxWidth: 768 });
 
   return (
     <div className="relative flex items-center justify-center w-screen h-screen">
       <main className="flex flex-col md:flex-row items-center justify-center h-[85vh]">
         <motion.section
-          initial={{ opacity: 0, y: 50, zIndex: 10 }}
-          animate={{ opacity: 1, y: 0, zIndex: 10 }}
-          transition={{ duration: 1.5 }}
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
           className="flex flex-col items-center md:items-start justify-center max-w-xs md:max-w-[34.5rem] md:-mt-20 md:basis-2/3 gap-6 py-8 md:gap-14 text-center md:text-left"
         >
-          <h1 className="text-4xl font-black md:text-7xl 2xl:text-8xl font-header">
+          <motion.h1 className="text-4xl font-black md:text-7xl 2xl:text-8xl font-header">
             Crafted with{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-tr from-accent to-primary">
               passion & precision
             </span>
-          </h1>
+          </motion.h1>
           <h2 className="text-lg md:text-2xl">
             Our whisky embodies the essence of <br />
             dedication and distinction.
@@ -34,9 +36,14 @@ const Hero2 = ({ heroImage }: Props) => {
         </motion.section>
 
         <motion.div
-          initial={{ opacity: 0, x: 0, zIndex: -10 }}
-          animate={{ opacity: 1, x: 0, zIndex: 0 }}
-          transition={{ duration: 1, delay: 1.5 }}
+          initial={{
+            opacity: 0,
+            x: isMobileOrTablet ? 0 : -200,
+            y: isMobileOrTablet ? 200 : 0,
+            zIndex: -10,
+          }}
+          animate={{ opacity: 1, x: 0, y: 0, zIndex: -10 }}
+          transition={{ duration: 1, delay: 1 }}
           className="relative h-full w-80 md:w-[34.5rem]"
         >
           <Image
