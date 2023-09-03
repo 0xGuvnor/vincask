05a48980c42307690eee2d61acc9062d56cd31e5
0xpaprhnds <yokeyeong@me.com>
2023-09-03 04:27:05 +0200

    Update AuditorBadge.tsx

--- src/components/AuditorBadge.tsx
+++ src/components/AuditorBadge.tsx
@@ -21,6 +21,7 @@ const AuditorBadge = ({ logo }: Props) => {
             quality={100}
             priority
             fill
+            sizes="(max-width: 768px) 50vw, 25vw"
             className="cursor-pointer rounded bg-violet-100 object-contain px-2 py-1 transition duration-300 ease-in-out hover:scale-105"
           />
         </a>
