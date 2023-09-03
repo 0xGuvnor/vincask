767cad71f35a44368d896cae06b9ca28f049cbaa
0xpaprhnds <yokeyeong@me.com>
2023-09-03 13:31:31 +0200

    Update TabButton.tsx

--- src/components/MintCard/TabButton.tsx
+++ src/components/MintCard/TabButton.tsx
@@ -14,7 +14,7 @@ const TabButton = ({ tab, setTab, tabValue, title }: Props) => {
       onClick={() => setTab(tabValue)}
       className="relative w-1/2 cursor-pointer select-none py-3 text-center transition duration-300 ease-in-out first:rounded-tl-xl last:rounded-tr-xl hover:bg-zinc-800 md:py-4"
     >
-      <p className="text-xs md:text-base">{title}</p>
+      <p className="text-sm md:text-base">{title}</p>
       <div className="absolute inset-x-0 bottom-0 z-0 h-[2px] w-full bg-zinc-800"></div>
       {tab === tabValue && (
         <motion.div
