775f71fd35ef724342a61bbc672d345268119ecc
0xpaprhnds <yokeyeong@me.com>
2023-09-05 00:42:06 +0200

    Update GridImage.tsx

--- src/components/WhiskyGrid/GridImage.tsx
+++ src/components/WhiskyGrid/GridImage.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 interface Props {
   big?: boolean;
@@ -7,6 +8,8 @@ interface Props {
 }
 
 const GridImage = ({ big, image, description }: Props) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div
       className={`${
@@ -25,7 +28,10 @@ const GridImage = ({ big, image, description }: Props) => {
         quality={100}
         fill
         draggable={false}
-        className="object-cover object-center transition delay-100 duration-300 ease-in-out group-hover:blur-md group-hover:brightness-50 group-hover:grayscale"
+        onLoadingComplete={() => setIsLoading(false)}
+        className={`${
+          isLoading ? "scale-110 blur-lg grayscale" : "scale-100"
+        } object-cover object-center transition delay-100 duration-300 ease-in-out group-hover:blur-md group-hover:brightness-50 group-hover:grayscale`}
       />
       <span className="font-outline-2 absolute inset-2 flex cursor-default select-none items-center justify-center opacity-0 transition duration-300 ease-in-out group-hover:opacity-100 group-hover:delay-100 md:text-lg">
         {description}
