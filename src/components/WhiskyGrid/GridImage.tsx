6893807031aed7aa9f177f4b16967d6095277c04
0xpaprhnds <yokeyeong@me.com>
2023-09-05 12:13:34 +0200

    Update GridImage.tsx

--- src/components/WhiskyGrid/GridImage.tsx
+++ src/components/WhiskyGrid/GridImage.tsx
@@ -31,7 +31,7 @@ const GridImage = ({ big, image, description }: Props) => {
         onLoadingComplete={() => setIsLoading(false)}
         className={`${
           isLoading ? "scale-110 blur-lg" : "scale-100"
-        } object-cover object-center transition delay-100 duration-300 ease-in-out group-hover:blur-md group-hover:brightness-50 group-hover:grayscale`}
+        } object-cover object-center transition delay-100 duration-300 ease-in-out group-hover:scale-95 group-hover:blur-md group-hover:brightness-50 group-hover:grayscale`}
       />
       <span className="font-outline-2 absolute inset-2 flex cursor-default select-none items-center justify-center opacity-0 transition duration-300 ease-in-out group-hover:opacity-100 group-hover:delay-100 md:text-lg">
         {description}
