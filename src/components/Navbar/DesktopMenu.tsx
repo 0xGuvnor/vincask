e6ed17b30807624ea7811c9cf33bf2128c7e7047
0xpaprhnds <yokeyeong@me.com>
2023-09-08 17:08:17 +0200

    Update DesktopMenu.tsx

--- src/components/Navbar/DesktopMenu.tsx
+++ src/components/Navbar/DesktopMenu.tsx
@@ -8,22 +8,22 @@ const DesktopMenu = () => {
   return (
     <div className="hidden items-center space-x-3 md:flex lg:space-x-6">
       {menuItems.map((menuItem) => (
-        <div
+        <Link
           key={menuItem.link}
+          href={menuItem.link}
           className="group relative cursor-pointer hover:text-primary-content hover:delay-100"
         >
-          <Link href={menuItem.link}>
-            <span
-              className={`${
-                router.pathname === menuItem.link &&
-                "text-primary transition-colors ease-in-out group-hover:text-primary-content"
-              }`}
-            >
-              {menuItem.title}
-            </span>
-          </Link>
+          <span
+            className={`${
+              router.pathname === menuItem.link &&
+              "text-primary transition-colors ease-in-out group-hover:text-primary-content"
+            }`}
+          >
+            {menuItem.title}
+          </span>
+
           <span className="absolute inset-x-0 bottom-0 -z-10 h-full w-full origin-bottom scale-y-[0.05] rounded-sm bg-primary transition duration-300 ease-in-out group-hover:scale-x-125 group-hover:scale-y-110"></span>
-        </div>
+        </Link>
       ))}
     </div>
   );
