d82e1823d794e29336c5aa294220304f648ecdf5
0xpaprhnds <yokeyeong@me.com>
2023-09-11 19:02:23 +0200

    Update TabButton.tsx

--- src/components/MintCard/TabButton.tsx
+++ src/components/MintCard/TabButton.tsx
@@ -12,7 +12,7 @@ const TabButton = ({ tab, tabValue, title }: Props) => {
 
   return (
     <li
-      onClick={() => router.push(`?method=${tabValue}`)}
+      onClick={() => router.replace(`?method=${tabValue}`)}
       className="relative w-1/2 cursor-pointer select-none py-3 text-center transition duration-300 ease-in-out first:rounded-tl-xl last:rounded-tr-xl hover:bg-zinc-800 md:py-4"
     >
       <p className="text-sm md:text-base">{title}</p>
