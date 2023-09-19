942d55a6ce21e5021858c6068450514951ad1224
0xpaprhnds <yokeyeong@me.com>
2023-09-19 19:29:45 +0200

    Removed AnimatePresence

--- src/components/Steps/Gallery.tsx
+++ src/components/Steps/Gallery.tsx
@@ -19,36 +19,28 @@ const Gallery = ({ selected, images }: Props) => {
 
   return (
     <section className="flex-1 lg:col-span-2">
-      <AnimatePresence initial={false} mode="popLayout">
-        <motion.div
-          key={id}
-          initial={{ opacity: 0, scaleX: 0 }}
-          animate={{
-            opacity: 1,
-            scaleX: 1,
-            transition: {
-              delay: 0.4,
-              duration: 0.4,
-            },
-          }}
-          exit={{
-            opacity: 0,
-            scaleX: 0,
-            transition: { duration: 0.4 },
-          }}
-          className="relative z-20 h-64 rounded-md ring ring-primary md:h-96 lg:h-full lg:rounded-lg"
-        >
-          <Image
-            src={images[6 + id]}
-            alt="GIF of step"
-            quality={100}
-            sizes="(max-width: 768px) 100vw, 40vw"
-            priority
-            fill
-            className="object-contain"
-          />
-        </motion.div>
-      </AnimatePresence>
+      <motion.div
+        key={id}
+        initial={{ opacity: 0, y: 100 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration: 0.5,
+          },
+        }}
+        className="relative z-20 h-64 rounded-md ring ring-primary md:h-96 lg:h-full lg:rounded-lg"
+      >
+        <Image
+          src={images[6 + id]}
+          alt="GIF of step"
+          quality={100}
+          sizes="(max-width: 768px) 100vw, 40vw"
+          priority
+          fill
+          className="object-contain"
+        />
+      </motion.div>
     </section>
   );
 };
