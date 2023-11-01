2ba3e5b8f3ae5627f9e6d94cb778737aea8de127
0xGuvnor <yokeyeong@me.com>
2023-11-01 19:15:58 +0100

    Update InfoPopover.tsx

--- src/components/MintCard/InfoPopover.tsx
+++ src/components/MintCard/InfoPopover.tsx
@@ -12,7 +12,7 @@ const InfoPopover = () => {
         <Popover.Content
           side="top"
           collisionPadding={20}
-          className="rounded-md bg-neutral text-sm shadow-2xl md:text-base"
+          className="rounded-md bg-neutral text-sm shadow-2xl focus:outline-none md:text-base"
         >
           <p className="mx-auto max-w-xs whitespace-pre-line px-4 py-2 md:max-w-sm">
             {`NFTs are randomly released in batches over time.
