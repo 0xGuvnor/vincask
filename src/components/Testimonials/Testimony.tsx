3d025d7f7481535ea46a7c3bd35b5d8b79f45866
0xGuvnor <yokeyeong@me.com>
2023-11-09 23:42:50 +0800

    Update Testimony.tsx

--- src/components/Testimonials/Testimony.tsx
+++ src/components/Testimonials/Testimony.tsx
@@ -25,21 +25,13 @@ const Testimony = ({ certImages }: Props) => {
 
       <div className="relative flex flex-col px-12 md:px-0">
         <VscQuote className="absolute -top-4 left-0 h-10 w-10 md:-left-[52px] md:-top-9 md:h-14 md:w-14 lg:-left-[69px] lg:-top-11 lg:h-[69px] lg:w-[69px]" />
-        <span className="text-justify leading-relaxed md:text-left 2xl:text-lg 2xl:leading-loose">
-          Our premium whisky stands apart from the rest, setting a new standard
-          of excellence in the industry. What separates our whisky from others
-          is the relentless pursuit of perfection at every stage of its
-          creation. From our carefully selected ingredients to our traditional
-          distillation methods, we leave no stone unturned in crafting a product
-          that is truly top-notch. Our attention to detail, unwavering
-          commitment to quality, and respect for time-honored techniques result
-          in a whisky that embodies exceptional character, complexity, and
-          smoothness. Each sip is a testament to our unwavering dedication to
-          producing a whisky that surpasses expectations and leaves a lasting
-          impression on the most discerning palates.
+        <span className="whitespace-pre-line text-justify leading-relaxed md:text-left 2xl:text-lg 2xl:leading-loose">
+          {`Our premium whisky stands apart from the rest, setting a new standard of excellence in the industry. What separates our whisky from others is the relentless pursuit of perfection at every stage of its creation. From our carefully selected ingredients to our traditional distillation methods, we leave no stone unturned in crafting a product that is truly top-notch. 
+          
+          Our attention to detail, unwavering commitment to quality, and respect for time-honored techniques result in a whisky that embodies exceptional character, complexity, and smoothness. Each sip is a testament to our unwavering dedication to producing a whisky that surpasses expectations and leaves a lasting impression on the most discerning palates.`}
         </span>
-        <span className="mt-4 text-lg">Logan Roy</span>
-        <span className="mb-4 italic">Distillery owner</span>
+        <span className="mt-4 2xl:text-lg">- Logan Roy</span>
+        <span className="mb-4 italic 2xl:text-lg">Distillery owner</span>
         <OwnershipCertModal certImages={certImages} />
       </div>
     </motion.section>
