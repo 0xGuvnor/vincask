057c40d6077e593cb5fddea72bac1c920e3d2961
0xGuvnor <yokeyeong@me.com>
2023-11-27 18:49:50 +0800

    Update BarItem.tsx

--- src/components/Comparison/BarItem.tsx
+++ src/components/Comparison/BarItem.tsx
@@ -18,25 +18,24 @@ const BarItem = ({ item, price, priceShort, length, primary }: Props) => {
       whileInView={{ width: `${length}%` }}
       viewport={{ once: true, amount: 0.6 }}
       transition={{ delay: 0.5, duration: 1 }}
-      //   style={{ width: `${length}%` }}
       className={`${
         primary ? "bg-primary" : "bg-neutral"
       } relative flex h-10 items-center justify-start rounded`}
     >
       <motion.p
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        initial={{ opacity: 0, y: 25 }}
+        whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.6 }}
-        transition={{ delay: 1.75, ease: "easeInOut" }}
+        transition={{ delay: 1.75, duration: 0.5, ease: "easeInOut" }}
         className={`${primary && "text-primary-content"} pl-2 lg:pl-4`}
       >
         {item}
       </motion.p>
       <motion.p
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        initial={{ opacity: 0, y: 25 }}
+        whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.6 }}
-        transition={{ delay: 1.75, ease: "easeInOut" }}
+        transition={{ delay: 1.75, duration: 0.5, ease: "easeInOut" }}
         className="absolute inset-y-0 -right-10 flex items-center md:-right-12 lg:-right-16"
       >
         {isMobile ? priceShort : price}
