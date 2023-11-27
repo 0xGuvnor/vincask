ee83b71e09beaac3598b30800c1f845e88020d12
0xGuvnor <yokeyeong@me.com>
2023-11-27 18:35:25 +0800

    Refactor + animations

--- src/components/Comparison/BarItem.tsx
+++ src/components/Comparison/BarItem.tsx
@@ -1,25 +1,47 @@
 import { useMediaQuery } from "react-responsive";
+import { motion } from "framer-motion";
 
 interface Props {
   item: string;
   price: string;
   priceShort: string;
   length: string;
+  primary?: boolean;
 }
 
-const BarItem = ({ item, price, priceShort, length }: Props) => {
+const BarItem = ({ item, price, priceShort, length, primary }: Props) => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
 
   return (
-    <div
+    <motion.div
+      initial={{ width: "0%" }}
+      whileInView={{ width: `${length}%` }}
+      viewport={{ once: true, amount: 0.6 }}
+      transition={{ delay: 0.5, duration: 1 }}
       //   style={{ width: `${length}%` }}
-      className={`relative flex h-10 w-[${length}%] items-center justify-start rounded bg-neutral`}
+      className={`${
+        primary ? "bg-primary" : "bg-neutral"
+      } relative flex h-10 items-center justify-start rounded`}
     >
-      <p className="pl-2 lg:pl-4">{item}</p>
-      <p className="absolute inset-y-0 -right-10 flex items-center md:-right-12 lg:-right-16">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.6 }}
+        transition={{ delay: 1.75, ease: "easeInOut" }}
+        className={`${primary && "text-primary-content"} pl-2 lg:pl-4`}
+      >
+        {item}
+      </motion.p>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.6 }}
+        transition={{ delay: 1.75, ease: "easeInOut" }}
+        className="absolute inset-y-0 -right-10 flex items-center md:-right-12 lg:-right-16"
+      >
         {isMobile ? priceShort : price}
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 };
 export default BarItem;
--- src/components/Comparison/index.tsx
+++ src/components/Comparison/index.tsx
@@ -1,16 +1,24 @@
-import { useMediaQuery } from "react-responsive";
+import useIsMounted from "@/hooks/useIsMounted";
 import BarItem from "./BarItem";
+import { motion } from "framer-motion";
 
 const Comparison = () => {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isMounted = useIsMounted();
 
+  if (!isMounted) return null;
   return (
     <section className="grid grid-cols-1 gap-10 lg:grid-cols-3">
-      <p className="whitespace-pre-line text-xl">
+      <motion.p
+        initial={{ opacity: 0, y: 100, filter: "blur(20px)" }}
+        whileInView={{ opacity: 1, y: 0, filter: "blur(0)" }}
+        viewport={{ once: true, amount: 0.6 }}
+        transition={{ type: "spring", duration: 0.5, bounce: 0.2 }}
+        className="whitespace-pre-line text-xl"
+      >
         {`With Port Ellen poised to reopen in 2024, there will be great fanfare for the distillery in the near future. 
         
-        It will be well beyond 2070 before another similar aged Port Ellen can be created. Even then, these old 70s distillates will be impossible to replicate and will be even more sought after by collectors and connoisseurs alike.`}
-      </p>
+        It will be well beyond 2070 before another similar aged Port Ellen can be created. Even then, these old '70s distillates will be impossible to replicate and will be even more sought after by collectors and connoisseurs alike.`}
+      </motion.p>
 
       <div className="relative col-span-2 flex flex-col justify-center gap-4 text-xs lg:text-sm">
         <BarItem
@@ -19,41 +27,37 @@ const Comparison = () => {
           priceShort="$58.8K"
           length="90"
         />
-
         <BarItem
           item="50 YO Balvenie"
           price="$55,477"
           priceShort="$55.5K"
-          length="80"
+          length="89.48"
         />
-
-        <div className="relative flex h-10 w-[65%] items-center justify-start rounded bg-neutral">
-          <p className="pl-2 lg:pl-4">50 YO Highland Park 2020</p>
-          <p className="absolute inset-y-0 -right-10 flex items-center md:-right-12 lg:-right-16">
-            {isMobile ? "$34.9K" : "$34,860"}
-          </p>
-        </div>
-        <div className="relative flex h-10 w-[50%] items-center justify-start rounded bg-neutral">
-          <p className="pl-2 lg:pl-4">50 YO Glengoyne</p>
-
-          <p className="absolute inset-y-0 -right-10 flex items-center md:-right-12 lg:-right-16">
-            {isMobile ? "$33.7K" : "$33,736"}
-          </p>
-        </div>
-        <div className="relative flex h-10 w-[45%] items-center justify-start rounded bg-neutral">
-          <p className="pl-2 lg:pl-4">47 YO Balvenie 1971 Cask 2855</p>
-          <p className="absolute inset-y-0 -right-10 flex items-center md:-right-12 lg:-right-16">
-            {isMobile ? "$32.7K" : "$32,748"}
-          </p>
-        </div>
-        <div className="relative flex h-10 w-[30%] items-center justify-start rounded bg-primary">
-          <p className="pl-2 text-primary-content lg:pl-4">
-            50 YO Port Ellen Cask P5X2
-          </p>
-          <p className="absolute inset-y-0 -right-10 flex items-center md:-right-12 lg:-right-16">
-            {isMobile ? "$19.9K" : "$19,880"}
-          </p>
-        </div>
+        <BarItem
+          item="50 YO Highland Park 2020"
+          price="$34,860"
+          priceShort="$34.9K"
+          length="56.23"
+        />
+        <BarItem
+          item="50 YO Glengoyne"
+          price="$33,736"
+          priceShort="$33.7K"
+          length="54.43"
+        />
+        <BarItem
+          item="47 YO Balvenie 1971 Cask 2855"
+          price="$32,748"
+          priceShort="$32.7K"
+          length="52.82"
+        />
+        <BarItem
+          item="50 YO Port Ellen Cask P5X2"
+          price="$19,880"
+          priceShort="$19.9K"
+          length="32.06"
+          primary
+        />
       </div>
     </section>
   );
