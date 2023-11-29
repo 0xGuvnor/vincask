631ffc6d31af34401282a2c74bc5b08bd7605a0f
0xGuvnor <yokeyeong@me.com>
2023-11-29 20:30:42 +0800

    Update ParallexItem.tsx

--- src/components/AboutPortEllen/ParallexItem.tsx
+++ src/components/AboutPortEllen/ParallexItem.tsx
@@ -112,7 +112,7 @@ const ParallexItem = ({ years, title, description, odd }: Props) => {
               }
             : { y: descriptionY, opacity: descriptionOpacity }
         }
-        className={`${odd && "order-first"} space-y-1 md:w-1/2`}
+        className={`${odd ? "order-first" : ""} space-y-1 md:w-1/2`}
       >
         <h3 className="text-2xl font-bold md:text-4xl">{title}</h3>
         <p>{description}</p>
